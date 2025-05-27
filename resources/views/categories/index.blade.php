9556584c89b3c50b48def52236df4b3efcf9f7f1
pratikshrees <prsh_csit2078@lict.edu.np>
2025-05-27 10:58:42 +0545

    edit,delete

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -15,8 +15,8 @@
             <th class="p-2 border border-grey-50">{{$category->order}}</th>
             <th class="p-2 border border-grey-50">{{$category->name}}</th>
             <th class="p-2 border border-grey-50">
-                <a href="" class="bg-blue-500 text-white px-2 py-1 rounded-md">Edit</a>
-                <a href=""  class="bg-red-500 text-white px-2 py-1 rounded-md">Delete</a>
+                <a href="{{route('categories.edit',$category->order)}}" class="bg-blue-500 text-white px-2 py-1 rounded-md">Edit</a>
+                <a href="{{route('categories.destroy',$category->order)}}"  class="bg-red-500 text-white px-2 py-1 rounded-md">Delete</a>
             </th>
         </tr>
         @endforeach
